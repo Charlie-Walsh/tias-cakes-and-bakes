cdd8bd461dc976d5eb05863c54c95657d7ce9941
Charlie-Walsh <charlie.walsh.141@gmail.com>
2024-02-26 21:31:27 +0000

    added media and css dirs, base css added for homepage and linked

--- tias_cakes_and_bakes/settings.py
+++ tias_cakes_and_bakes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ph0(lfp9lj=^qmhos_xv#oe&%!-*py3!mqr6a$c5c#wq+4!4-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-charliewals-tiascakesan-u7ah4kiy1rj.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-charliewals-tiascakesan-lyv5u7kn1u5.ws-eu108.gitpod.io']
 
 
 # Application definition
@@ -148,6 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
