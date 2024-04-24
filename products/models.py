3fd9931e8fef6f2e8dcdbbb499d61e823a215e3f
Charlie-Walsh <charlie.walsh.141@gmail.com>
2024-04-24 20:46:56 +0000

    product images added

--- products/models.py
+++ products/models.py
@@ -3,13 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    freindly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    def get_freindly_name(self):
-        return self.freindly_name
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
